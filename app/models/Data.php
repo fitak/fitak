504067a902ebf958391444492db08068be4ddc92
Jan Tvrdik <jan.tvrdik@gmail.com>
2012-11-24 23:29:26 +0100

    message cleaning moved to TemplateHelpers

--- app/models/Data.php
+++ app/models/Data.php
@@ -252,8 +252,6 @@ class Data extends BaseModel
         foreach( $topics as $topic )
         {
             // $topic->likesData = $this->getLikes( $topic->id );
-            $topic->message = $this->cleanMessage( $topic->message );
-            $topic->from_name = stripslashes( $topic->from_name );
 
             if( isset( $comments[$topic->id] ) )
             {
@@ -262,8 +260,6 @@ class Data extends BaseModel
                 {
                     $comment->topic = $topic;
                     $comment->marked = in_array( $comment->id, $marked );
-                    $comment->message = $this->cleanMessage( $comment->message );
-                    $comment->from_name = stripslashes( $comment->from_name );
                 }
             }
             else
@@ -273,51 +269,6 @@ class Data extends BaseModel
         }
 
         return $topics;
-    }
-
-    // change links, strip slashes and HTML
-    private function cleanMessage( $message )
-    {
-        return $this->urlChange( stripslashes( str_replace( '\n', '<br />', htmlspecialchars( $message ) ) ) );
-    }
-
-    // find and replace all links in text, links will be shorter (clever cut), titled and active
-    private function urlChange( $inText )
-    {
-        // define an url regular exression pattern:
-        $urlPattern = "/(https?:\/\/|www.)(www.)?([-a-z0-9]*[a-z0-9]\.)(\bcom\b|\bbiz\b|\bgov\b|\bmil\b|\bnet\b|\borg\  b|[a-z][a-z]|[a-z][a-z]\.[a-z][a-z])\/?([a-zA-Z0-9]*)?([a-zA-Z0-9_\-\.\?=\/&%#;~\+]*)?/";
-        // get all matches
-        preg_match_all( $urlPattern, $inText, $temp );
-        $temp = $temp[0]; // an array of all urls
-
-        $urlTag = array();
-        foreach( $temp as &$url )
-        {
-            $urlShort = str_replace( "http://", "", $url );
-            while( strlen( $urlShort ) > 35 )
-            { // limit is set to 35
-                if( is_bool( strpos( $urlShort, "/" ) ) )
-                { // are there any natural places to cut the link?
-                    $urlShort = substr( $urlShort, 0, 30 );
-                }
-                else
-                { // find a good place to cut the link
-                    $newLength = max( strrpos( $urlShort, "/" ), strrpos( $urlShort, "?" ), strrpos( $urlShort, "#" ) );
-                    $urlShort = substr( $urlShort, 0, $newLength );
-                }
-                $urlShort = $urlShort . "...";
-            }
-            $urlTag[] = "<a href=\"$url\" title=\"$url\">$urlShort</a>";
-        }
-        if( 0 < sizeof( $temp ) )
-        {
-            // if there are any urls in the text, replace them with the new code
-            return strtr( $inText, array_combine( $temp, $urlTag ) );
-        }
-        else
-        {
-            return $inText;
-        }
     }
 
     // return array of variations for input word
