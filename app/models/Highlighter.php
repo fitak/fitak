65a67e14995f01046a852a80a4e4bb203deb5738
Mikulas <rullaf@gmail.com>
2014-09-30 14:59:09 +0200

    elastic: remove highlights from tags

--- app/models/Highlighter.php
+++ app/models/Highlighter.php
@@ -3,19 +3,25 @@
 class Highlighter
 {
 
-	protected static $regex;
+	protected static $normalize;
+	protected static $tags;
 
 	public function __construct()
 	{
-		if (!self::$regex)
+		if (!self::$normalize)
 		{
-			self::$regex = self::buildRegex();
+			self::$normalize = self::buildRegex();
+			self::$tags = self::buildTagsRegex();
 		}
 	}
 
 	/**
 	 * 1/ Replaces elasticsearch markup with html markup.
-	 * 2/ Default elasticsearch highlighter does not highlight
+	 *
+	 * 2/ Partial tag highlights highlight whole tag.
+	 * For example "[BI->PA1<]" translates to "[BI-PA1]", without highlight.
+	 *
+	 * 3/ Default elasticsearch highlighter does not highlight
 	 * stop words and separates by white space.
 	 * String ">foo< stopword >bar<" with >< denoting highlights,
 	 * will be converted to ">foo stopword bar<".
@@ -25,8 +31,8 @@ class Highlighter
 	 */
 	public function processHighlight($input)
 	{
-		$input = preg_replace(self::$regex, '$1', $input);
-
+		$input = preg_replace(self::$normalize, '$1', $input);
+		$input = preg_replace(self::$tags, '[$1$3$5]', $input);
 		$input = htmlentities($input, ENT_NOQUOTES);
 		$input = str_replace(ElasticSearch::HIGHLIGHT_START, '<span class="highlight">', $input);
 		$input = str_replace(ElasticSearch::HIGHLIGHT_END, '</span>', $input);
@@ -46,6 +52,15 @@ class Highlighter
 			'(\W*\s*(' . implode('|', $words) . ')\s*\W*)' .
 			preg_quote(ElasticSearch::HIGHLIGHT_START, '~') .
 			'~';
+	}
+
+	private static function buildTagsRegex()
+	{
+		return '~\\[([^\\[]{0,10})' .
+			'(' . preg_quote(ElasticSearch::HIGHLIGHT_START, '~') . ')' .
+			'([^\\]]{1,15})' .
+			'(' . preg_quote(ElasticSearch::HIGHLIGHT_END, '~') . ')' .
+			'([^\\]]){0,10}\\]~';
 	}
 
 }
