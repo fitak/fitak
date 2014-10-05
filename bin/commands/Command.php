6c2499ff8d9d1ac5d1accb9e0fd438745225a451
Mikulas <rullaf@gmail.com>
2014-10-05 21:49:27 +0200

    refactor es-regenerate-index to es:reindex command

---
+++ bin/commands/Command.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Bin\Commands;
+
+use Kdyby\Console\Application;
+use Nette\DI\Container;
+use Symfony\Component\Console\Command\Command as SCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class Command extends SCommand
+{
+
+	/**
+	 * @var InputInterface
+	 */
+	protected $in;
+
+	/**
+	 * @var OutputInterface|ConsoleOutput
+	 */
+	protected $out;
+
+	/**
+	 * @var Container
+	 */
+	private $container;
+
+	public function setApplication(Application $application = NULL)
+	{
+		parent::setApplication($application);
+		$application->setAutoExit(TRUE);
+
+		/** @var Container $container */
+		$this->container = $this->getHelper('container')->getContainer();
+		$this->container->callInjects($this);
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$this->in = $input;
+		$this->out = $output;
+
+		return $this->container->callMethod([$this, 'invoke']);
+	}
+
+}
