957e42c541a4dead6ace132122b7c0b6f266ee34
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-10-05 17:29:25 +0200

    migrations: typo in news table

---
+++ migrations/dummy-data/2014-10-05-003-news-typo.sql
@@ -0,0 +1,3 @@
+UPDATE `news`
+SET `message` = 'Nastupujete v září 2014 na FIT ČVUT?\n<a href=\"https://www.facebook.com/groups/fit14/\">Sem!</a>\n<br>\nChcete trolovat lidi, kteří odpověděli na první otázku „ano“?\n<a href=\"https://www.facebook.com/groups/fit14/\">Sem!</a>'
+WHERE `id` = '1';
