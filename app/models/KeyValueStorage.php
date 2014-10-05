f968fcb243fc2b67cf05c06044780961187c912e
Mikulas <rullaf@gmail.com>
2014-10-05 23:15:24 +0200

    add persistent key value storage service

---
+++ app/models/KeyValueStorage.php
@@ -0,0 +1,45 @@
+<?php
+
+use Nette\Database\Context as Database;
+use Nette\Utils\Json;
+
+
+class KeyValueStorage
+{
+
+	const FACEBOOK_ACCESS_TOKEN = 'facebook.access_token';
+	const CRAWLER_SINCE = 'crawler.since';
+
+	/**
+	 * @var Database
+	 */
+	private $db;
+
+	public function __construct(Database $db)
+	{
+		$this->db = $db;
+	}
+
+	public function save($key, $value)
+	{
+		$ser = Json::encode($value);
+		$this->db->query('
+			INSERT INTO `key_value` (`key`, `value`) VALUES (?, ?)
+			ON DUPLICATE KEY UPDATE `value` = ?
+		', $key, $ser, $ser);
+	}
+
+	/**
+	 * @param string $key
+	 * @return NULL|mixed
+	 */
+	public function get($key)
+	{
+		$row = $this->db->query('
+			SELECT `value` FROM `key_value` WHERE `key` = ?
+		', $key)->fetch();
+
+		return $row ? Json::decode($row->value) : NULL;
+	}
+
+}
