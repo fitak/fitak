989e38031849989d0c2ae1bad4101274b60980e8
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-10-05 15:13:17 +0200

    RouterFactory: shorter urls for AuthPresenter

--- app/misc/RouterFactory.php
+++ app/misc/RouterFactory.php
@@ -1,6 +1,7 @@
 <?php
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
+use Nextras\Routing\StaticRouter;
 
 class RouterFactory extends Nette\Object
 {
@@ -21,8 +22,18 @@ class RouterFactory extends Nette\Object
 		$flags = $this->useHttps ? Route::SECURED : 0;
 
 		$router = new RouteList();
-		$router[] = new Route('index.php', 'Homepage:default', $flags | Route::ONE_WAY);
-		$router[] = new Route('stream/', 'Search:stream', $flags);
+		$router[] = new StaticRouter(['Homepage:default' => 'index.php'], $flags | Route::ONE_WAY);
+		$router[] = new StaticRouter(
+			[
+				'Search:stream' => 'stream/',
+				'Auth:signIn' => 'sign-in',
+				'Auth:signOut' => 'sign-out',
+				'Auth:signUp' => 'sign-up',
+				'Auth:signUpAfter' => 'sign-up-after',
+				'Auth:signUpConfirm' => 'sign-up-confirm',
+			],
+			$flags
+		);
 		$router[] = new Route('<presenter>/<action>[/<id>]', 'Homepage:default', $flags);
 
 		return $router;
