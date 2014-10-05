0ed21b8115253aaa1a2410bcba6850c36eb71902
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-10-05 16:13:25 +0200

    SearchResponse: Iterator interface replace with IteratorAggregate

--- app/models/SearchResponse.php
+++ app/models/SearchResponse.php
@@ -3,7 +3,7 @@
 use Nette\Object;
 
 
-class SearchResponse extends Object implements Iterator
+class SearchResponse extends Object implements IteratorAggregate
 {
 
 	/**
@@ -15,8 +15,6 @@ class SearchResponse extends Object implements Iterator
 	 * @var array
 	 */
 	protected $highlights;
-
-	private $key = 0;
 
 	/**
 	 * @var int
@@ -59,67 +57,19 @@ class SearchResponse extends Object implements Iterator
 	}
 
 	/**
-	 * Return the current element
-	 *
-	 * @link http://php.net/manual/en/iterator.current.php
-	 * @return mixed Can return any type.
-	 */
-	public function current()
-	{
-		return $this->topics[$this->key];
-	}
-
-	/**
-	 * Move forward to next element
-	 *
-	 * @link http://php.net/manual/en/iterator.next.php
-	 * @return void Any returned value is ignored.
-	 */
-	public function next()
-	{
-		$this->key++;
-	}
-
-	/**
-	 * Return the key of the current element
-	 *
-	 * @link http://php.net/manual/en/iterator.key.php
-	 * @return mixed scalar on success, or null on failure.
-	 */
-	public function key()
-	{
-		return $this->key;
-	}
-
-	/**
-	 * Checks if current position is valid
-	 *
-	 * @link http://php.net/manual/en/iterator.valid.php
-	 * @return boolean The return value will be casted to boolean and then evaluated.
-	 * Returns true on success or false on failure.
-	 */
-	public function valid()
-	{
-		return isset($this->topics[$this->key]);
-	}
-
-	/**
-	 * Rewind the Iterator to the first element
-	 *
-	 * @link http://php.net/manual/en/iterator.rewind.php
-	 * @return void Any returned value is ignored.
-	 */
-	public function rewind()
-	{
-		$this->key = 0;
-	}
-
-	/**
 	 * @return int
 	 */
 	public function getTotal()
 	{
 		return $this->total;
+	}
+
+	/**
+	 * @return ArrayIterator|Traversable
+	 */
+	public function getIterator()
+	{
+		return new \ArrayIterator($this->topics);
 	}
 
 }
