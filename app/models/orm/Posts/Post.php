ae60b828ec9eb9c37506ab831f83b6de51560f34
Mikulas <rullaf@gmail.com>
2014-10-07 17:31:07 +0200

    add post getParsedTags(), getMessageWithoutTags()

--- app/models/orm/Posts/Post.php
+++ app/models/orm/Posts/Post.php
@@ -3,9 +3,11 @@
 namespace Fitak;
 
 use DateTime;
+use Nette\Utils\Strings;
 use Nextras\Orm;
 use Nextras\Orm\Relationships\OneHasMany;
 use Nextras\Orm\Relationships\ManyHasMany;
+use Tags;
 
 
 /**
@@ -37,5 +39,27 @@ class Post extends Orm\Entity\Entity
 	const TYPE_VIDEO = 'video';
 	const TYPE_LINK = 'link';
 	const TYPE_PHOTO = 'photo';
+
+	/**
+	 * @var Tags
+	 * @inject
+	 */
+	public $tagParser;
+
+	/**
+	 * @return string[][] [string[] $cleanTags, string[] $originalTags]
+	 */
+	public function getParsedTags()
+	{
+		return $this->tagParser->extractTags($this->message);
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getMessageWithoutTags()
+	{
+		return Strings::trim($this->tagParser->separateMessage($this->message)[1]);
+	}
 
 }
