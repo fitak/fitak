7f5b5bbe72af332c84b3c3e5d52a4ff619ee7b7c
Mikulas <rullaf@gmail.com>
2014-10-07 17:01:02 +0200

    refactor Tags service
    Now it's possible to get message without tags.

--- app/models/Tags.php
+++ app/models/Tags.php
@@ -1,7 +1,7 @@
 <?php
 
-use Fitak\Tag;
 use Nette\Utils\Strings;
+
 
 class Tags extends BaseModel
 {
@@ -13,24 +13,42 @@ class Tags extends BaseModel
 		$this->orm->tags->persistAndFlush($tag);
 	}
 
+	/**
+	 * @param string $input
+	 * @return NULL[]|string[] [NULL|string $rawTags, NULL|string $content]
+	 */
+	public function separateMessage($input)
+	{
+		$match = Strings::match($input, '/^\s*(?<tag_list>\[\s*[\pL\d._-]+\s*\](?:\s*(?&tag_list))?)(?<message>.*)/u');
+		if (!$match)
+		{
+			return [NULL, NULL];
+		}
+		return [$match['tag_list'], $match['message']];
+	}
+
+	/**
+	 * @param string $input raw tags
+	 * @return string[][] [string[] $cleanTags, string[] $originalTags]
+	 */
+	public function parse($input)
+	{
+		if (!$input)
+		{
+			return [];
+		}
+
+		$tags = Strings::trim($input, '[] ');
+		$tags = Strings::split($tags, '/\]\s*\[/');
+		return [array_map('Nette\Utils\Strings::webalize', $tags), $tags];
+	}
+
 	// extract tags ([tag1][tag2]....) from the start of input
 	// return array(cleanTags, originalTags)
 	public function extractTags($input)
 	{
-		$matches = Strings::match($input, '/^\s*(?<tag_list>\[\s*[\pL\d._-]+\s*\](?:\s*(?&tag_list))?)/u');
-
-		if ($matches)
-		{
-			$tags = Strings::trim($matches['tag_list'], '[] ');
-			$tags = Strings::split($tags, '/\]\s*\[/');
-			$tags = [array_map('Nette\Utils\Strings::webalize', $tags), $tags];
-		}
-		else
-		{
-			$tags = [];
-		}
-
-		return $tags;
+		list($tags) = $this->separateMessage($input);
+		return $this->parse($tags);
 	}
 
 }
