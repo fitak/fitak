f9c9066d759b12e6d1ee2a2ecabd364197ee205b
Mikulas <rullaf@gmail.com>
2014-10-07 18:11:29 +0200

    remove obsolete data functions

--- app/models/Data.php
+++ app/models/Data.php
@@ -20,20 +20,6 @@ class Data extends BaseModel
 		$this->elastic = $elastic;
 	}
 
-	/**
-	 * @param  int $id
-	 * @return DateTime|NULL|FALSE
-	 */
-	public function getUpdatedTime($id)
-	{
-		return $this->db->fetchSingle('
-			SELECT updated_time
-			FROM data
-			WHERE id = %i', $id, '
-			LIMIT 1
-		');
-	}
-
 	// get all topics + comments
 	public function getAllTopics($length, $offset)
 	{
@@ -60,6 +46,25 @@ class Data extends BaseModel
 		if ($justTopics) $sql->where("parent_id IS NULL");
 
 		return $sql->fetchSingle();
+	}
+
+	public function searchFulltext(SearchRequest $request, $length, $offset)
+	{
+		$response = $this->elastic->fulltextSearch($request, $length, $offset);
+		$map = [];
+		foreach ($response['hits']['hits'] as $hit)
+		{
+			$map[$hit['_id']] = isset($hit['highlight']['message_raw'][0]) ? $hit['highlight']['message_raw'][0] : NULL;
+		}
+
+		if (!$map)
+		{
+			return new SearchResponse([], [], 0);
+		}
+
+		$topics = $this->db->query('SELECT * FROM data WHERE id IN %in ORDER BY Field([id], ?)', array_keys($map), array_keys($map));
+		$result = $this->processRawResult($topics);
+		return new SearchResponse($result, $map, $response['hits']['total']);
 	}
 
 	protected function processRawResult($result)
@@ -98,89 +103,6 @@ class Data extends BaseModel
 		return $topics;
 	}
 
-	// main search function
-	// topics and comments are in the same data table, specific by parent_id column
-	public function search(SearchRequest $request, $length, $offset)
-	{
-		$sql = $this->db->select("data.id, data.parent_id")
-			->from("data")
-			->leftJoin("groups")
-			->on("data.group_id = groups.id");
-
-		$muted = $this->getMatchedIdByTags($this->mutedTags);
-		if ($muted)
-		{
-			$sql->where("data.id NOT IN %in", $muted)
-				->where("data.parent_id NOT IN %in", $muted);
-		}
-
-		// use of filters on query
-		$this->addSearchCondition($sql, $request);
-
-		// sort by time or relevance
-		if ($request->query != "")
-		{
-			$sql->orderBy("MATCH(data.message) AGAINST (%s) DESC", $request->query);
-		}
-
-		$result = $sql->fetchAll($offset, $length);
-
-		return $this->processRawResult($result);
-	}
-
-	public function searchFulltext(SearchRequest $request, $length, $offset)
-	{
-		$response = $this->elastic->fulltextSearch($request, $length, $offset);
-		$map = [];
-		foreach ($response['hits']['hits'] as $hit)
-		{
-			$map[$hit['_id']] = isset($hit['highlight']['message_raw'][0]) ? $hit['highlight']['message_raw'][0] : NULL;
-		}
-
-		if (!$map)
-		{
-			return new SearchResponse([], [], 0);
-		}
-
-		$topics = $this->db->query('SELECT * FROM data WHERE id IN %in ORDER BY Field([id], ?)', array_keys($map), array_keys($map));
-		$result = $this->processRawResult($topics);
-		return new SearchResponse($result, $map, $response['hits']['total']);
-	}
-
-	// number of results by search
-	public function searchCount(SearchRequest $request)
-	{
-		$sql = $this->db->select("count(*)")
-			->from("data")
-			->leftJoin("groups")
-			->on("data.group_id = groups.id")
-			->where("data.id NOT IN %in", $this->getMatchedIdByTags($this->mutedTags))
-			->where("data.parent_id NOT IN %in", $this->getMatchedIdByTags($this->mutedTags));
-
-		$this->addSearchCondition($sql, $request);
-
-		return $sql->fetchSingle();
-	}
-
-	// get likes at topic by id
-	public function getLikes($id)
-	{
-		$result = $this->db->query("
-            SELECT from_name, from_id
-            FROM likes
-            WHERE message_id = %i
-            ORDER BY id
-            ", $id
-		);
-		$result = $result->fetchAll();
-		foreach ($result as $item)
-		{
-			$item->from_name = stripslashes($item->from_name);
-		}
-
-		return $result;
-	}
-
 	// get array of ids topics, which are labeled by string array of input tags
 	public function getMatchedIdByTags($tags)
 	{
@@ -190,46 +112,6 @@ class Data extends BaseModel
 			->on("data_tags.tags_id = tags.id")
 			->where("tags.name IN %in", $tags)
 			->fetchPairs();
-	}
-
-	// add search condition corresponding with given search request
-	private function addSearchCondition(DibiFluent $sql, SearchRequest $request)
-	{
-		if ($request->query == "" && $request->from == "")
-		{
-			// limit results to topics
-			$sql->where("data.parent_id IS NULL");
-		}
-		else
-		{
-			if ($request->query != "")
-			{
-				$sql->where("MATCH(data.message) AGAINST (%s IN BOOLEAN MODE)", $request->query);
-			}
-
-			if ($request->from != "")
-			{
-				$sql = $sql->where("data.from_name LIKE %~like~", $request->from);
-				// protection of hidden names in closed/secret groups
-				$sql = $sql->where("groups.closed = 0");
-			}
-		}
-
-		if (count($request->tags))
-		{
-			$taggedPostsId = $this->getMatchedIdByTags($request->tags);
-			$sql = $sql->where("data.id IN %in", $taggedPostsId);
-		}
-
-		if (count($request->groups))
-		{
-			$sql->where("data.group_id IN %in", $request->groups);
-		}
-
-		if ($request->since > 0)
-		{
-			$sql->where("data.created_at >= %t", $request->since);
-		}
 	}
 
 	// returns comments for given list of topics
