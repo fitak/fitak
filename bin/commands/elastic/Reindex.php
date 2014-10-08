2441e3af20c67a434109c152ae6c6fd909f47c16
Mikulas <rullaf@gmail.com>
2014-10-08 11:40:31 +0200

    refactor es:reindex command to use es updater

--- bin/commands/elastic/Reindex.php
+++ bin/commands/elastic/Reindex.php
@@ -4,7 +4,8 @@ namespace Bin\Commands\Elastic;
 
 use Bin\Commands\Command;
 use ElasticSearch;
-use Nette\Database\Context as Database;
+use Fitak\ElasticSearchUpdater;
+use Fitak\RepositoryContainer;
 use Symfony\Component\Console\Helper\ProgressBar;
 
 
@@ -18,34 +19,20 @@ class Reindex extends Command
 			->setDescription('Drops current index, sets new mapping and add all data to index');
 	}
 
-	public function invoke(ElasticSearch $elastic, Database $db)
+	public function invoke(ElasticSearch $elastic, RepositoryContainer $orm, ElasticSearchUpdater $updater)
 	{
 		$this->out->writeln('<info>Erasing index...</info>');
 		$elastic->setupIndices();
 		$this->out->writeln('<info>Mapping recreated</info>');
 
-		$res = $db->query('
-			SELECT
-				`id`, `message`, `group_id`, `likes`,
-				If(`parent_id` IS NULL, 1, 0) `is_topic`,
-				Unix_Timestamp(`created_time`) `timestamp`,
-				`from_name` `author`
-			FROM `data`
-		');
-
 		$this->out->writeln('<info>Indexing...</info>');
-		$pb = new ProgressBar($this->out, $res->getRowCount());
+		$posts = $orm->posts->findAll();
+
+		$pb = new ProgressBar($this->out, $posts->count());
 		$pb->start();
-		foreach ($res as $row)
+		foreach ($posts as $post)
 		{
-			$elastic->addToIndex(ElasticSearch::TYPE_CONTENT, $row['id'], [
-				'message' => $row['message'],
-				'author' => $row['author'],
-				'is_topic' => (bool) $row['is_topic'],
-				'created_time' => $row['timestamp'],
-				'group' => $row['group_id'],
-				'likes' => $row['likes'],
-			]);
+			$updater->onAfterInsert($post);
 			$pb->advance();
 		}
 		$pb->finish();
