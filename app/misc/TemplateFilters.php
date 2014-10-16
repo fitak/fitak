9cb505b8246de1977618ae55c8c19b493b2dab8a
Jan Tvrdik <jan.tvrdik@gmail.com>
2014-10-17 00:07:50 +0200

    TemplateFilters: rewriten urlsToLinks() from scratch [closes #52]

--- app/misc/TemplateFilters.php
+++ app/misc/TemplateFilters.php
@@ -5,8 +5,7 @@ namespace Fitak;
 use Latte;
 use Highlighter;
 use Nette;
-use Nextras\Application\LinkFactory;
-use Tags;
+use Nette\Utils\Strings;
 
 
 class TemplateFilters extends Nette\Object
@@ -34,42 +33,59 @@ class TemplateFilters extends Nette\Object
 		return $this->highlighter->processHighlight($highlight);
 	}
 
+	/**
+	 * Based on Nette\Utils\Validators::isUrl in Nette (http://nette.org)
+	 * Licensed under New BSD License (https://github.com/nette/utils/blob/v2.2.2/license.md)
+	 * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+	 */
 	public function urlsToLinks($input)
 	{
-		// define an url regular expression pattern:
-		$urlPattern = '/(https?:\/\/|www.)(www.)?([-a-z0-9]*[a-z0-9]\.)(\bcom\b|\bbiz\b|\bgov\b|\bmil\b|\bnet\b|\borg\  b|[a-z][a-z]|[a-z][a-z]\.[a-z][a-z])\/?([a-zA-Z0-9]*)?([a-zA-Z0-9_\-\.\?=\/&%#!;~\+]*)?/';
-		// get all matches
-		preg_match_all($urlPattern, $input, $temp);
-		$temp = $temp[0]; // an array of all urls
+		$alpha = "a-z\x80-\xFF";
+		$domain = "[0-9$alpha](?:[-0-9$alpha]{0,61}[0-9$alpha])?";
+		$topDomain = "[$alpha](?:[-0-9$alpha]{0,17}[$alpha])?";
+		$pattern = "(^https?://(?:(?:$domain\\.)*$topDomain|\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}|\\[[0-9a-f:]{3,39}\\])(:\\d{1,5})?(/\\S*)?\\z)i";
 
-		$urlTag = [];
-		foreach ($temp as &$url)
+		return Strings::replace($input, $pattern, function ($matches) {
+			$url = $matches[0];
+			$text = $this->getTextualUrl($url);
+			return "<a href=\"$url\" title=\"$url\">$text</a>";
+		});
+	}
+
+	/**
+	 * Based on TexyLinkModule::textualUrl in Texy! (http://texy.info).
+	 * Licensed under New BSD License (https://github.com/dg/texy/blob/v2.6/license.txt)
+	 * Copyright (c) 2004 David Grudl (http://davidgrudl.com)
+	 */
+	private function getTextualUrl($url)
+	{
+		// parse_url() in PHP damages UTF-8 - use regular expression
+		if (!preg_match('~^(?:(?P<scheme>[a-z]+):)?(?://(?P<host>[^/?#]+))?(?P<path>(?:/|^)(?!/)[^?#]*)?(?:\?(?P<query>[^#]*))?(?:#(?P<fragment>.*))?()$~', $url, $parts))
 		{
-			$urlShort = str_replace("http://", "", $url);
-			while (strlen($urlShort) > 35)
-			{ // limit is set to 35
-				if (is_bool(strpos($urlShort, "/")))
-				{ // are there any natural places to cut the link?
-					$urlShort = substr($urlShort, 0, 30);
-				}
-				else
-				{ // find a good place to cut the link
-					$newLength = max(strrpos($urlShort, "/"), strrpos($urlShort, "?"), strrpos($urlShort, "#"));
-					$urlShort = substr($urlShort, 0, $newLength);
-				}
-				$urlShort = $urlShort . "...";
-			}
-			$urlTag[] = "<a href=\"$url\" title=\"$url\">$urlShort</a>";
+			return $url;
 		}
-		if (0 < sizeof($temp))
+
+		$res = '';
+		if ($parts['host'] !== '')
 		{
-			// if there are any urls in the text, replace them with the new code
-			return strtr($input, array_combine($temp, $urlTag));
+			$res .= $parts['host'];
 		}
-		else
+
+		if ($parts['path'] !== '')
 		{
-			return $input;
+			$res .= (iconv_strlen($parts['path'], 'UTF-8') > 16 ? ("/\xe2\x80\xa6" . iconv_substr($parts['path'], -12, 12, 'UTF-8')) : $parts['path']);
 		}
+
+		if ($parts['query'] !== '')
+		{
+			$res .= iconv_strlen($parts['query'], 'UTF-8') > 4 ? "?\xe2\x80\xa6" : ('?' . $parts['query']);
+		}
+		elseif ($parts['fragment'] !== '')
+		{
+			$res .= iconv_strlen($parts['fragment'], 'UTF-8') > 4 ? "#\xe2\x80\xa6" : ('#' . $parts['fragment']);
+		}
+
+		return $res;
 	}
 
 }
--- tests/cases/misc/TemplateFiltersTest.phpt
+++ tests/cases/misc/TemplateFiltersTest.phpt
@@ -27,19 +27,39 @@ class TemplateFiltersTest extends Tester\TestCase
 	/**
 	 * @dataProvider getUrlsToLinksData
 	 */
-	public function testUrlsToLinks($expected, $input)
+	public function testUrlsToLinks($input, $expected)
 	{
 		Assert::same($expected, $this->templateFilters->urlsToLinks($input));
 	}
 
 	public function getUrlsToLinksData()
 	{
+		$hellip = "\xe2\x80\xa6";
 		return [
-			['simple', 'simple'],
-			['<html>', '<html>'],
-			['<a href="http://example.com" title="http://example.com">example.com</a>', 'http://example.com'],
-			['<a href="http://example.com/very-long-url-very-long-url" title="http://example.com/very-long-url-very-long-url">example.com...</a>', 'http://example.com/very-long-url-very-long-url'],
-			['<a href="http://example.com/aaa/bbb/ccc/ddd/eee/fff/ggg" title="http://example.com/aaa/bbb/ccc/ddd/eee/fff/ggg">example.com/aaa/bbb/ccc/ddd/eee...</a>', 'http://example.com/aaa/bbb/ccc/ddd/eee/fff/ggg'],
+			[
+				'simple',
+				'simple',
+			],
+			[
+				'<html>',
+				'<html>',
+			],
+			[
+				'http://example.com',
+				"<a href=\"http://example.com\" title=\"http://example.com\">example.com</a>",
+			],
+			[
+				'http://example.com/very-long-url-very-long-url',
+				"<a href=\"http://example.com/very-long-url-very-long-url\" title=\"http://example.com/very-long-url-very-long-url\">example.com/{$hellip}ery-long-url</a>",
+			],
+			[
+				'http://example.com/aaa/bbb/ccc/ddd/eee/fff/ggg',
+				"<a href=\"http://example.com/aaa/bbb/ccc/ddd/eee/fff/ggg\" title=\"http://example.com/aaa/bbb/ccc/ddd/eee/fff/ggg\">example.com/{$hellip}/eee/fff/ggg</a>",
+			],
+			[
+				'http://cs.wikipedia.org/wiki/Třešeň',
+				"<a href=\"http://cs.wikipedia.org/wiki/Třešeň\" title=\"http://cs.wikipedia.org/wiki/Třešeň\">cs.wikipedia.org/wiki/Třešeň</a>",
+			],
 		];
 	}
 
