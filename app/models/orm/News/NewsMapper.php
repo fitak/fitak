99487735b5e3ed5791714a9285975bca6fe598af
Matej Herman <atomicmail@seznam.cz>
2015-11-27 17:17:17 +0100

    Move findRecent() method for news from mapper to repository
    It is better to have the query DB independent, if it is possible, so now findRecent() is rewritten with
    Nextras ORM methods and putted to Repository.

--- app/models/orm/News/NewsMapper.php
+++ app/models/orm/News/NewsMapper.php
@@ -8,14 +8,4 @@ use Nextras\Orm\Mapper\Dbal;
 class NewsMapper extends Mapper
 {
 
-	public function findRecent()
-	{
-		return $this->connection->query('
-			SELECT *
-			FROM `news`
-			WHERE NOW() < `created` + INTERVAL 2 WEEK
-			ORDER BY `created` DESC
-		');
-	}
-
 }
--- app/models/orm/News/NewsRepository.php
+++ app/models/orm/News/NewsRepository.php
@@ -6,10 +6,13 @@ use Nextras\Orm\Collection\ICollection;
 use Nextras\Orm\Repository\Repository;
 
 
-/**
- * @method ICollection findRecent()
- */
 class NewsRepository extends Repository
 {
-
+	public function findRecent()
+	{
+		$newsInterval = new \DateTime('-2 week');
+		return $this->findAll()
+			->findBy(['this->created>=' => $newsInterval])
+			->orderBy('created', ICollection::DESC);
+	}
 }
