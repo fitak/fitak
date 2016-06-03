7fc0bc641fd63bf4d59c2b33431ddeef84c49505
Matej Herman <atomicmail@seznam.cz>
2016-06-03 12:34:27 +0200

    Move SearchPresenter to Homepage

---
+++ app/presenters/OldHomepagePresenter.php
@@ -0,0 +1,36 @@
+<?php
+
+use Fitak\TagCloudControl;
+
+/**
+ * @author     Vojtech Miksu <vojtech@miksu.cz>
+ */
+class OldHomepagePresenter extends BasePresenter
+{
+    protected function startup()
+    {
+        parent::startup();
+        $this->requireLogin('Pro přístup k obsahu je nutné se přihlásit.');
+    }
+
+	public function renderDefault()
+	{
+
+		$this->template->news = $this->orm->news->findRecent()->fetchAll();
+	}
+
+	protected function createComponentSearchForm()
+	{
+		$form = new SearchForm($this->orm, $advanced = FALSE);
+		$form->onSuccess[] = callback($form, 'submitted');
+
+		return $form;
+	}
+
+	protected function createComponentTagCloud()
+	{
+		$trending = $this->orm->tags->findTrending();
+		return new TagCloudControl($trending);
+	}
+
+}
