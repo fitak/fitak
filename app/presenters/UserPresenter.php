e7af7cd0021adb285ebefb358442612ec1a4b0dc
Matej Herman <atomicmail@seznam.cz>
2016-06-05 22:31:48 +0200

    Fit static templates into containers

--- app/presenters/UserPresenter.php
+++ app/presenters/UserPresenter.php
@@ -1,4 +1,5 @@
 <?php
+use Nette\Application\UI;
 
 class UserPresenter extends BasePresenter
 {
@@ -6,28 +7,41 @@ class UserPresenter extends BasePresenter
 	public $facebook;
 
 	/** @var */
+    private $user;
 
-	public function actionDefault()
-	{
-	}
 
-	public function renderDefault()
-	{
+    protected function createComponentUserSettingsForm() {
+        $this->user = $this->getLoggedInUser();
+        $user = $this->user;
 
-	}
+        $form = new UI\Form;
+        $form->addText('name', 'Jméno:')
+            ->setDefaultValue($user->name);
+        $form->addText('profile_picture', 'Avatar:')
+            ->setDefaultValue($user->profilePicture);
+        $form->addPassword('password', 'Heslo:');
+        $form->addPassword('password_again', 'Heslo pro kontrolu:')
+            ->addRule($form::EQUAL, 'Zadané hesla se musí shodovat, zkontroluj si překlepy.', $form['password']);
+        $form->addSubmit('login', 'Změnit nastavení');
+        $form->onSuccess[] = [$this, 'formSubmitted'];
+        return $form;
+    }
 
-	public function actionProfile()
-	{
+    public function formSubmitted(UI\Form $form, $values)
+    {
+        $values = $values = $form->getValues(TRUE);
+        $user = $this->user;
+        $user->name = $values['name'];
+        $user->profilePicture = $values['profile_picture'];
 
-	}
+        if ($values['password']) {
+            $user->passwordHash = password_hash($values['password'], PASSWORD_BCRYPT);
+        }
 
-	public function renderProfile()
-	{
+        $this->orm->users->persistAndFlush($user);
 
-	}
-
-	public function actionRemoveFacebookAccount()
-	{
-	}
+        $this->flashMessage('Nastavení úspěšně změněno', 'success');
+        $this->redirect('User:');
+    }
 }
 
